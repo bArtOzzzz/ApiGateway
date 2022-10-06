c1f672d03e133e986de5a43a07deb59e9eacd842
bArtOzzzz <breakyourbrain55@gmail.com>
2022-10-06 13:00:47 +0300

    Finish Ocelot

---
+++ GetwayApi/Extention/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using GetwayApi.Decorator;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Authorization;
+
+namespace GetwayApi.Extention
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection DecorateClaimAuthoriser(this IServiceCollection services)
+        {
+            var serviceDescriptor = services.First(x => x.ServiceType == typeof(IClaimsAuthorizer));
+            services.Remove(serviceDescriptor);
+
+            var newServiceDescriptor = new ServiceDescriptor(serviceDescriptor.ImplementationType, serviceDescriptor.ImplementationType, serviceDescriptor.Lifetime);
+            services.Add(newServiceDescriptor);
+
+            services.AddTransient<IClaimsAuthorizer, ClaimAuthorizerDecorator>();
+
+            return services;
+        }
+    }
+}
--- GetwayApi/Program.cs
+++ GetwayApi/Program.cs
@@ -1,19 +1,43 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using GetwayApi.Extention;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Service.Extention;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add Configuration for Ocelot
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json")
-    .AddEnvironmentVariables();
+                     .AddJsonFile("ocelot.json")
+                     .AddEnvironmentVariables();
 
+// Add Ocelot
 builder.Services.AddOcelot();
 
-builder.Services.AddCustomJwtAuthentication();
+// Add Extension
+builder.Services.DecorateClaimAuthoriser();
+
+// Add JWT Token validation
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.RequireHttpsMetadata = false;
+    option.SaveToken = true;
+    option.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("JsonWebApiTokenWithSwaggerAuthorizationAuthenticationAspNetCore"))
+    };
+});
 
 // Enable CORS
 builder.Services.AddCors(options =>
